1ee92a1efc3f9bdf27c7de06f1e0a84e2037940e
Oleksii Protasov <oleksi.protasov@gmail.com>
2022-11-30 13:44:55 +0200

    step3

---
+++ contacts.js
@@ -0,0 +1,33 @@
+
+
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+    // ...твій код
+  }
+  
+  function getContactById(contactId) {
+    // ...твій код
+  }
+  
+  function removeContact(contactId) {
+    // ...твій код
+  }
+  
+  function addContact(name, email, phone) {
+    // ...твій код
+  }
+
+  const todoUser = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+  };
+
+  module.exports=todoUser;
+  
